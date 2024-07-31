4b756c36f863d460ac8133d980a7ba036375a92d
k15460 <k15460@ktkbank.com>
2024-07-31 20:24:05 +0530

    Arrow Functions

--- export.js
+++ export.js
@@ -1,10 +1,27 @@
 // export
-function sum(a, b) {
-    return a + b;
-}
 
-module.exports = {
-   sum
-};
+// standard function
+// function sum(a, b) {
+//     return a + b;
+// }
 
-console.log("Process: ",process)
+// module.exports = {
+//     sum
+//  };
+
+// Array function
+// const sum = (a, b) => a + b; //cz it is one line no need of const sum = ( a, b ) => { return a + b; } ;
+// module.exports = {
+//     sum
+// };
+
+// no parameters
+// const sum = (  ) => a + b; 
+
+// 1 parameter
+// const sum = s => a + b; 
+
+// can also export as below
+exports.sum = (a, b) => a + b;
+
+console.log("Process: ", process)
